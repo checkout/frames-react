4a1bb25bcf4f61d3dd290bfce93ac816a1ef95aa
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2022-05-04 14:34:37 +0100

    Add support for single line

--- src/frames/index.tsx
+++ src/frames/index.tsx
@@ -2,4 +2,5 @@ export * from './types/types';
 export * from './components/card-number';
 export * from './components/expiry-date';
 export * from './components/cvv';
+export * from './components/card-frame';
 export * from './Frames';
