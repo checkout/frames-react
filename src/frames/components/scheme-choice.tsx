ed9ba545384e1ce045a732724c2a5b3f02171db8
ioan-ghisoi-cko <ioan.ghisoi@checkout.com>
2022-06-22 14:20:21 +0100

    Add support for CB and add scheme_local and preferred_scheme

---
+++ src/frames/components/scheme-choice.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SCHEME_CHOICE_FRAME } from '../config/config';
+
+export const SchemeChoice: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
+    className,
+    ...props
+}) => <div className={classNames(SCHEME_CHOICE_FRAME, className)} {...props} />;
--- src/frames/config/config.tsx
+++ src/frames/config/config.tsx
@@ -3,3 +3,4 @@ export const CARD_NUMBER_FRAME = 'card-number-frame';
 export const EXPIRY_DATE_FRAME = 'expiry-date-frame';
 export const CVV_FRAME = 'cvv-frame';
 export const CARD_FRAME = 'card-frame';
+export const SCHEME_CHOICE_FRAME = 'scheme-choice-frame';
