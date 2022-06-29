7cd383268539eee068110363a78de5d228664f77
Ioan Ghisoi <ioan.ghisoi@checkout.com>
2022-06-29 13:49:04 +0100

    Update dependencies and workflows (#21)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
+const element = document.getElementById('root');
+const root = createRoot(element!);
+
+root.render(
+    <StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </StrictMode>
 );
